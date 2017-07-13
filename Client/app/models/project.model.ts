7461d999ace01f2dca2d355614a8671306b59116
Web Expert <web.expert@aol.com>
2017-07-13 06:32:34 +0300

    udpate for project.model.ts

--- Client/app/models/project.model.ts
+++ Client/app/models/project.model.ts
@@ -1,7 +1,12 @@
+import { CategoryModel } from './category.model';
 export class ProjectModel {
-    constructor(public id: number, public name: string, )
-    {
-
+    constructor(public id: number,
+        public name: string,
+        public exerpt: string,
+        public dateAdded: Date,
+        public categories: CategoryModel[],
+        public runCount: number,
+        public assetCount: number
+    ) {
     }
 }
-export interface 
